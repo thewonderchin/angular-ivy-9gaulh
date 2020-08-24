551ca47f3f0a13865b5e3ccaae60e33d5cdaaff4
thewonderchin <corey.banaszak@gmail.com>
2020-08-24 10:34:47 -0400

    1 file modified

--- src/app/tree.component.ts
+++ src/app/tree.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'tree',
-  template: '<h1>Hello {{name}}!</h1>',
-  styles: ['h1 { font-family: Lato; }']
+  template: `<h1>Hello {{name}}!</h1>`,
+  styles: [`h1 { font-family: Lato; }`]
 })
 export class TreeComponent {
   @Input() name: string;
